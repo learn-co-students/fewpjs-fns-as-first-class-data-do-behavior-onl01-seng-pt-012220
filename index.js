7c9650573b05258a9daeedc14c55a4e48826c05b
Charlie Pelton <charliejp0311@gmail.com>
2020-07-22 18:28:40 -0500

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,29 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  if (time.length == 5) {
+    hour = time.slice(0,2);
+  }else{
+    hour = time.slice(0,1);
+  }
+
+  if (hour<12) {
+    greeting = "Good Morning";
+  } else if (hour<17) {
+    greeting = "Good Afternoon";
+  }else{
+    greeting = "Good Evening";
+  }
+  
+  return greeting;
+}
 /* Write your implementation of displayMessage() */
+function displayMessage() {
+  let b = document.getElementsByTagName('button');
+  b[0].addEventListener('click', function () {
+    let input = document.querySelector('input');
+    let greeting = document.querySelector("#greeting");
+    greeting.textContent = greet(input.value);
+  });
+}
